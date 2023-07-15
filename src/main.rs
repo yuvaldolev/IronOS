81b5373325559643c7bf7470cfd9de9e124137f8
Yuval Dolev <yuvaldolev2@gmail.com>
2023-07-15 23:15:12 +0300

    feat: create a freestanding Rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// Invoked by the bootloader to start the kernel.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// Invoked on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
