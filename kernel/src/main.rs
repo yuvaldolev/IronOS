a11656265242637f90f2fb1f46b77d52dfe32871
Yuval Dolev <yuvaldolev2@gmail.com>
2023-08-05 20:21:57 +0300

    feat: kernel logging

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,17 +3,36 @@
 
 use core::panic::PanicInfo;
 
+use bootloader_api::info::FrameBufferInfo;
 use bootloader_api::BootInfo;
+use bootloader_x86_64_common::logger::LockedLogger;
+use conquer_once::spin::OnceCell;
 
 bootloader_api::entry_point!(kernel_main);
 
+static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
+
 /// Invoked by the bootloader to start the kernel.
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
-        for byte in framebuffer.buffer_mut() {
-            *byte = 0x90;
-        }
-    }
+    // Free the doubly wrapped framebuffer from the boot info struct.
+    let frame_buffer_optional = &mut boot_info.framebuffer;
+
+    // Free the wrapped framebuffer from the FFI-safe abstraction provided by bootloader_api.
+    let frame_buffer_option = frame_buffer_optional.as_mut();
+
+    // Unwrap the framebuffer.
+    let frame_buffer = frame_buffer_option.unwrap();
+
+    // Extract the framebuffer info and, to satisfy the borrow checker, clone it.
+    let frame_buffer_info = frame_buffer.info().clone();
+
+    // Get the framebuffer's mutable raw byte slice.
+    let raw_frame_buffer = frame_buffer.buffer_mut();
+
+    // Finally, initialize the logger using the last two variables.
+    init_logger(raw_frame_buffer, frame_buffer_info);
+
+    log::info!("Hello, Kernel Mode!");
 
     loop {}
 }
@@ -22,4 +41,10 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+fn init_logger(buffer: &'static mut [u8], info: FrameBufferInfo) {
+    let logger = LOGGER.get_or_init(move || LockedLogger::new(buffer, info, true, false));
+    log::set_logger(logger).expect("Logger already set");
+    log::set_max_level(log::LevelFilter::Trace);
 }
